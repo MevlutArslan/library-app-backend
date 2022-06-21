0eb69239e2eb46658a142827f03d0a6caca8f99a
MevlutArslan <mevlut12098@gmail.com>
2022-06-21 18:59:38 +0200

    test BookRepository

--- src/main/java/com/example/libraryappbackend/book/BookRepository.java
+++ src/main/java/com/example/libraryappbackend/book/BookRepository.java
@@ -4,11 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
 
-    List<Book> findByAuthor(String author);
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByAuthorName(String authorName);
 
     List<Book> findByTitle(String title);
-
-//    List<Book> findByPublishDate(String date);
 }
---
+++ src/test/java/com/example/libraryappbackend/book/BookRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.example.libraryappbackend.book;
+
+import com.example.libraryappbackend.author.Author;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    void shouldFindBooksByAuthor() {
+        Author author = new Author("Morgan", "Freeman");
+
+        Book book = new Book("exampleTitle", author,"2nd of June");
+        bookRepository.save(book);
+
+        List<Book> booksByAuthor = bookRepository.findByAuthorName(book.getAuthor().getName());
+
+        assertThat(booksByAuthor.contains(book)).isTrue();
+    }
+
+    @Test
+    void findByTitle() {
+        Author author = new Author("Morgan", "Freeman");
+
+        Book book = new Book("Lucy", author,"2nd of June");
+        bookRepository.save(book);
+
+        List<Book> booksWithTitle = bookRepository.findByTitle(book.getTitle());
+
+        assertThat(booksWithTitle.contains(book)).isTrue();
+    }
+}
