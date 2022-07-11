784c4c12c3c09bfabe3807d13463023bd58a5696
MevlutArslan <mevlut12098@gmail.com>
2022-07-11 15:07:38 +0200

    remove unnecessary exceptions

--- src/main/java/com/example/libraryappbackend/exceptions/BookDoesntExistException.java
+++
@@ -1,7 +0,0 @@
-package com.example.libraryappbackend.exceptions;
-
-public class BookDoesntExistException extends Exception{
-    public BookDoesntExistException(Long id){
-        super("Book with id : " + id + " does not exist in our system");
-    }
-}
